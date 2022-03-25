346894de7aef9b5bdf81b73c9a5e6cce9b6a8518
Taufik Januar <taufikjanuar35@gmail.com>
2022-03-25 20:27:02 +0700

    refactor #time 1h #comment refactoring setup fiber config, adding route not found handler

--- infrastructure/http.go
+++ infrastructure/http.go
@@ -4,10 +4,9 @@ import (
 	"github.com/PickHD/pickablog/application"
 	"github.com/PickHD/pickablog/helper"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-// ServeHTTP is function to start the apps infra in HTTP mode
+// ServeHTTP is wrapper function to start the apps infra in HTTP mode
 func ServeHTTP(app *application.App) *fiber.App {
 	//call setup router
 	setupRouter(app)
@@ -19,14 +18,21 @@ func ServeHTTP(app *application.App) *fiber.App {
 func setupRouter(app *application.App) {
 	var dep = application.SetupDependencyInjection(app)
 
-	app.Application.Use(cors.New())
  	api := app.Application.Group("/api")
-
-	v1 := api.Group("/v1", func (ctx *fiber.Ctx) error {
-		ctx.Set("Version","v1")
-		return ctx.Next()
+	{
+		v1 := api.Group("/v1", func (ctx *fiber.Ctx) error {
+			ctx.Set("Version","v1")
+			return ctx.Next()
+		})
+		
+		{
+			v1.Get("/health",dep.HealthCheckController.HealthCheck)
+			v1.Options("/health",helper.OptionsHandler)
+		}
+	}
+	
+	// handler for route not found
+	app.Application.Use(func(c *fiber.Ctx) error {
+   	 	return helper.ResponseFormatter[any](c,404,nil,"Route not found",nil)
 	})
-
-	v1.Get("/health",dep.HealthCheckController.HealthCheck)
-	v1.Options("/health",helper.OptionsHandler)
 }
